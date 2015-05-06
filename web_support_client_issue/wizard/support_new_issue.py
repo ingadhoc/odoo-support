21f761423591d220b2b22da3887eefe2df8f33fe
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-05-06 01:25:28 -0300

    IMP message

--- web_support_client_issue/wizard/support_new_issue.py
+++ web_support_client_issue/wizard/support_new_issue.py
@@ -75,6 +75,4 @@ class support_new_issue_wizzard(models.TransientModel):
         issue_id = active_contract.create_issue(vals, self.attachment_ids)
         return self.env['warning_box'].info(
             title=_('Issue succesfully loaded'),
-            message=_('For your reference and if you contact support by\
-                another channel:\n\
-                Issue ID: %s') % (issue_id))
+            message=_('For your reference and if you contact support by another channel, issue ID: %s') % (issue_id))
