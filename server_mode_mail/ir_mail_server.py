9dd7a1630b79755b729b84dcb1cddf1e83678c5f
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-18 16:20:47 -0300

    FIX pep

--- server_mode_mail/ir_mail_server.py
+++ server_mode_mail/ir_mail_server.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#/#############################################################################
+##############################################################################
 #
 #    BizzAppDev
 #    Copyright (C) 2015-TODAY bizzappdev(<http://www.bizzappdev.com>).
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#/#############################################################################
+##############################################################################
 
 from openerp.tools.translate import _
 from openerp import models
