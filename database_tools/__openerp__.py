f19b93f086af3983239bb4cda6f46ec3bf9693f4
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-08-20 14:21:16 -0300

    IMP version on database tools

--- database_tools/__openerp__.py
+++ database_tools/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Database Tools",
-    "version": "1.0",
+    "version": "8.0.2.0",
     'author':  'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     # "category": "Accounting",
