da7bc35334f59bd598e478c9ccb1ad5a9a1793b9
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-09-24 10:06:00 -0300

    IMP increase manifest version

--- database_tools/__openerp__.py
+++ database_tools/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Database Tools",
-    "version": "8.0.2.0.0",
+    "version": "8.0.2.0.1",
     'author':  'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     # "category": "Accounting",
