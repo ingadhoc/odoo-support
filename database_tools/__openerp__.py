c3512ebcfceaaf9a15d17c154e00fa7f3766cb47
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-08-22 18:14:06 -0300

    IMP varios

--- database_tools/__openerp__.py
+++ database_tools/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Database Tools",
-    "version": "8.0.2.3.0",
+    "version": "8.0.2.4.0",
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
