9f7d0654cdebc521e57335df27247ca9713255c8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-09-07 16:05:42 -0300

    REF

--- web_support_client/static/src/js/chat_widgets.js
+++ web_support_client/static/src/js/chat_widgets.js
@@ -13,13 +13,14 @@ openerp.web_support_client = function (instance) {
                     if (!values.user_remote_partner_uuid){
                         return;
                     }
-                    (function(t,a,l,k,u,s,_){if(!t[u]){t[u]=function(){(t[u].q=t[u].q||[]).push(arguments)},t[u].l=1*new Date();s=a.createElement(l),_=a.getElementsByTagName(l)[0];s.async=1;s.src=k;_.parentNode.insertBefore(s,_)}})(window,document,'script','//www.talkus.io/plugin.js','talkus');
+                    (function(t,a,l,k,u,s,e){if(!t[u]){t[u]=function(){(t[u].q=t[u].q||[]).push(arguments)},t[u].l=1*new Date();s=a.createElement(l),e=a.getElementsByTagName(l)[0];s.async=1;s.src=k;e.parentNode.insertBefore(s,e)}})(window,document,'script','//www.talkus.io/plugin.js','talkus');
                     if(values.talkus_image_url)
                         talkus('loadingImage', values.talkus_image_url);
                     if(values.user_image)
                         user_img = instance.session.url('/web/user/image', {user_id: values.user_id});
                     else
                         user_img = "";
+                    // talkus('position', 'left');
                     talkus('create', values.talkusID);
                     talkus('identify', { id: values.user_remote_partner_uuid, name: values.user_name, email: values.user_email, picture: user_img});
                 });
