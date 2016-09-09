dc951d8ff759fb1a1c27b70fad0ee410acc0d5d8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-09-09 11:33:44 -0300

    IMP new chat widget

--- adhoc_modules_server/README.rst
+++ adhoc_modules_server/README.rst
@@ -47,7 +47,7 @@ Known issues / Roadmap
 
 * Links documentación a modulos
 * Llevar documentación a clientes
-* Agregar linea en rojo si cantidad teorica distinta de real. Agregar un booleano en la vista lista tmb
+* Agregar linea en rojo si cantidad teorica distinta de real. Agregar un booleano en la vista lista tmb (el tema es los productos que no se leen, los computamos con 1?)
 * ver de agregar reglas de incompatibilidad entre modulos (ver como hace odoo en v9 de temas)
 * ver modulos instalados y no categorizados
 * Actualizar contratos con un boton, mostrar con un campo related el precio de lista actual (segun lista de precios del contrato)
--- web_support_client/static/src/js/chat_widgets.js
+++ web_support_client/static/src/js/chat_widgets.js
@@ -13,16 +13,24 @@ openerp.web_support_client = function (instance) {
                     if (!values.user_remote_partner_uuid){
                         return;
                     }
-                    (function(t,a,l,k,u,s,e){if(!t[u]){t[u]=function(){(t[u].q=t[u].q||[]).push(arguments)},t[u].l=1*new Date();s=a.createElement(l),e=a.getElementsByTagName(l)[0];s.async=1;s.src=k;e.parentNode.insertBefore(s,e)}})(window,document,'script','//www.talkus.io/plugin.js','talkus');
-                    if(values.talkus_image_url)
-                        talkus('loadingImage', values.talkus_image_url);
+                    // old snippet
+                    // (function(t,a,l,k,u,s,e){if(!t[u]){t[u]=function(){(t[u].q=t[u].q||[]).push(arguments)},t[u].l=1*new Date();s=a.createElement(l),e=a.getElementsByTagName(l)[0];s.async=1;s.src=k;e.parentNode.insertBefore(s,e)}})(window,document,'script','//www.talkus.io/plugin.js','talkus');
+                    // new snippet
+                    (function(t,a,l,k,u,s,e){if(!t[u]){t[u]=function(){(t[u].q=t[u].q||[]).push(arguments)},t[u].l=1*new Date();s=a.createElement(l),e=a.getElementsByTagName(l)[0];s.async=1;s.src=k;e.parentNode.insertBefore(s,e)}})(window,document,'script','https://www.talkus.io/plugin.beta.js','talkus');
+                    // if(values.talkus_image_url)
+                    //     talkus('loadingImage', values.talkus_image_url);
                     if(values.user_image)
                         user_img = instance.session.url('/web/user/image', {user_id: values.user_id});
                     else
                         user_img = "";
                     // talkus('position', 'left');
-                    talkus('create', values.talkusID);
-                    talkus('identify', { id: values.user_remote_partner_uuid, name: values.user_name, email: values.user_email, picture: user_img});
+
+                    // old snippet
+                    // talkus('create', values.talkusID);
+                    // talkus('identify', { id: values.user_remote_partner_uuid, name: values.user_name, email: values.user_email, picture: user_img});
+
+                    // new snippet
+                    talkus('init', values.talkusID, {id: values.user_remote_partner_uuid, name: values.user_name, email: values.user_email, picture: user_img});
                 });
             };
         this.update_promise = this.update_promise.then(fct, fct);
