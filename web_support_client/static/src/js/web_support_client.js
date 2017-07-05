d1884882e4cb1bd9fe037ffcb75e2dd92dcda294
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2017-07-05 12:38:05 -0300

    FIX to introduce url with uuid and remote_uid (#28)

--- web_support_client/static/src/js/web_support_client.js
+++ web_support_client/static/src/js/web_support_client.js
@@ -2,6 +2,9 @@ odoo.define('web_support_client.menu_links', function(require) {
 "use strict";
 var UserMenu = require('web.UserMenu');
 var Model = require('web.Model');
+var session = require('web.session');
+
+
 UserMenu.include({
         on_menu_service_portal: function() {
             new Model("res.users")
@@ -12,7 +15,10 @@ UserMenu.include({
             // window.open('https://www.adhoc.com.ar/doc/how-to', '_blank');
         },
         on_menu_help_and_doc: function() {
-            window.open('https://www.adhoc.com.ar/doc/624', '_blank');
+            var P = new Model('ir.config_parameter');
+            P.call('get_param', ['database.uuid']).then(function(dbuuid) {
+                window.open('https://www.adhoc.com.ar/doc/624/624/' + dbuuid + '/' + session.uid, '_blank');
+            });            
         },
     });
 });
