a3627e2eb0c3b025aa37669e3473ca1ece70cb59
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-10-01 09:27:54 -0300

    [REM] deprecated database_tools module

--- database_tools/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import database_backup_preserve_rule
-from . import database
-from . import database_backup
--- database_tools/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import db_database_backup_now_wizard
